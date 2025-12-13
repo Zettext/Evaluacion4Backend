b71872afcd803446432cdc4abd46fa1c08052e46
Zettext <javieralejandrosanchezleiva@gmail.com>
2025-12-12 21:00:51 -0300

    Prueba 7

--- api/models.py
+++ api/models.py
@@ -19,7 +19,7 @@ class Sensor(models.Model):
     mac_address = models.CharField(max_length=50, unique=True, verbose_name="MAC Address")
     modelo = models.CharField(max_length=50)
     estado = models.CharField(max_length=20, choices=ESTADOS, default='inactivo')
-    departamento = models.ForeignKey(Departamento, on_delete=models.SET_NULL, related_name='sensores')
+    departamento = models.ForeignKey(Departamento, on_delete=models.SET_NULL,null=True, related_name='sensores')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
